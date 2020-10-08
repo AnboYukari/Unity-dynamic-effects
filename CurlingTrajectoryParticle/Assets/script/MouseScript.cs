e642faa45902fcc75eedc0940684d57c70b56158
AnboYukari <b1018129@gmail.com>
2020-10-08 12:28:34 +0900

    [ADD]Add dynamic effects

---
+++ CurlingTrajectoryParticle/Assets/script/MouseScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+    void Start () {
+	}
+	[SerializeField] Transform particleObj;
+	RaycastHit hit;
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		bool is_hit = Physics.Raycast (ray, out hit, 1000);
+		if (is_hit) {
+			particleObj.transform.position = hit.point;
+			particleObj.rotation = 
+				Quaternion.FromToRotation (transform.forward, hit.normal);
+		}
+	}
+}
